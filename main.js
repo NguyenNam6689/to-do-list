aaafdce0453237af9a87d1ad9b04c94ab364ebbd
NguyenNam6689 <nhnam.12c2nvtroi@gmail.com>
2023-09-06 10:05:23 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,57 +1,89 @@
-let btnAddEle = document.querySelector('button')
-let taskName = document.querySelector('#content')
+const btnAddEle = document.querySelector('button'); // Nút thêm công việc
+const taskName = document.querySelector('#content'); // Ô nhập nội dung công việc
+const resultEle = document.querySelector('#result'); // Kết quả hiển thị danh sách công việc
 
-let tasks = getTaskFromLocalStorage ()
-renderTasks(tasks)
-btnAddEle.addEventListener('click',function (){
-    if (!taskName.value){
-        alert('Vui lòng nhập tên công việc !!')
-        return false;
+btnAddEle.addEventListener('click', handleButtonClick);
+
+// Hiển thị danh sách công việc ban đầu
+renderTasks();
+
+function handleButtonClick() {
+    // Lấy giá trị nhập vào, loại bỏ khoảng trắng đầu và cuối
+    const inputValue = taskName.value.trim();
+    if (!inputValue) {
+        alert('Vui lòng nhập tên công việc !!');
+        return;
     }
-    let tasksId = this.getAttribute('id') // lấy giá trị thuốc tính id từ function editTask
-    let tasks = getTaskFromLocalStorage () // Lấy danh sách các task
-    let task = {name : taskName.value} // tạo một đối tượng task mới với thuộc tính name được lấy từ giá trị của trường taskName.
-    if (tasksId == 0 || tasksId) { 
-        tasks[tasksId] = task  //cập nhật một phần tử trong mảng tasks tại vị trí cụ thể được chỉ định bởi tasksId
-        this.removeAttribute('id')
+
+    const taskId = btnAddEle.getAttribute('data-id');  // Lấy giá trị id từ thuộc tính data-id của nút thêm công việc
+    const tasks = getTasksFromLocalStorage();
+    const task = { name: inputValue };
+
+    // Kiểm tra nếu taskId đã tồn tại, thì cập nhật công việc, ngược lại thêm công việc mới
+    if (taskId !== null) {
+        updateTask(taskId, task, tasks);
     } else {
-        tasks.push(task)  
+        addTask(task, tasks);
     }
-    taskName.value=''
-    localStorage.setItem('tasks',JSON.stringify(tasks)) // trước khi lưu trữ danh sách được chuyển đổi thành chuỗi JSON
-    renderTasks(tasks)
-})
-function editTask (id){
-    let tasks = getTaskFromLocalStorage ()
-    if (tasks.length > 0) {
-        taskName.value=tasks[id].name
-        btnAddEle.setAttribute('id',id)
-    }
+
+    taskName.value = '';
+    renderTasks();
 }
-function deleteTask (id){
+
+function updateTask(id, updatedTask, tasks) {
+    tasks[id] = updatedTask;// Cập nhật nội dung công việc theo id
+    btnAddEle.removeAttribute('data-id'); // Xóa thuộc tính data-id trên nút thêm công việc để quay trở lại chế độ thêm mới
+    saveTasksToLocalStorage(tasks);
+}
+
+function addTask(newTask, tasks) {
+    tasks.push(newTask);
+    saveTasksToLocalStorage(tasks);
+}
+
+function deleteTask(id) {
     if (confirm("Bạn có muốn xoá không ?")) {
-        let tasks = getTaskFromLocalStorage()
-        tasks.splice(id,1) // dùng để xoá id với đối số là 1 chính là số phần tử bị xoá
-        localStorage.setItem('tasks',JSON.stringify(tasks))
-        renderTasks(tasks)
+        const tasks = getTasksFromLocalStorage();
+        tasks.splice(id, 1); // Xoá công việc theo id
+        saveTasksToLocalStorage(tasks);
+        renderTasks();
     }
 }
-function renderTasks (tasks=[]) {
+
+function renderTasks() {
+    const tasks = getTasksFromLocalStorage();
     let content = '<ul>'
 
-    
-    tasks.forEach((tasks, index) => {
+    tasks.forEach((task, index) => {
         content += `<li>
-                <div class="task-name">${tasks.name}</div>
-                <a href="#" onclick="editTask(${index})">Sửa</a>
-                <a href="#" onclick="deleteTask(${index})">Xoá</a>
-         </li>`
-    })
+            <div class="task-name">${task.name}</div>
+            <a href="#" onclick="editTask(${index})">Sửa</a>
+            <a href="#" onclick="deleteTask(${index})">Xoá</a>
+         </li>`;
+    });
 
-    content += '</ul>'
-    document.querySelector('#result').innerHTML = content
+    content += '</ul>';
+    resultEle.innerHTML = content;
 }
 
-function getTaskFromLocalStorage (){
-    return localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) :[]; // check dữ liệu từ storage có null không nếu không sẽ trả về dữ liệu từ chuỗi JSON thành 1 đối tượng JS
+function editTask(id) {
+    const tasks = getTasksFromLocalStorage();  
+    const task = tasks[id]; // Lấy công việc cần chỉnh sửa
+    
+    // Hiển thị tên công việc trong ô nhập và cập nhật thuộc tính data-id trên nút thêm công việc để cập nhật
+    if (task) {
+        taskName.value = task.name;
+        btnAddEle.setAttribute('data-id', id);
+    }
+}
+
+function getTasksFromLocalStorage() {
+    // Lấy danh sách công việc từ Local Storage hoặc trả về mảng rỗng nếu không có dữ liệu
+    const storedTasks = localStorage.getItem('tasks');
+    return storedTasks ? JSON.parse(storedTasks) : [];
+}
+
+function saveTasksToLocalStorage(tasks) {
+    // Lưu danh sách công việc vào Local Storage
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
